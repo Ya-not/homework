270746b2603893c0c6b732868f2df262fd29e309
Yanot <ya1998@icloud.com>
2023-01-30 00:39:24 +0300

    Добавили прогармму

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+String[] array = new string[3] {"soul", "war", "it"};
+String[] result = new string[array.Length];
+
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result[i] = array[i];
+        Console.WriteLine(result[i]);
+    }
+}
